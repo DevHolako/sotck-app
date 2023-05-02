06ed9da822077d6fbf16818ade2ee169f5e1b769
DevHolako <holakonoob@gmail.com>
2023-05-02 19:53:03 +0100

    routes v1

---
+++ src/routes/index.tsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter } from "react-router-dom";
+import Client from "../pages/Client";
+import Layout from "../pages/Layout";
+import Login from "../pages/Login";
+import Stock from "../pages/Stock";
+import Invoice from "../pages/Invoice";
+import RequireLogin from "../helpers/RequireLogin";
+import NotFound from "../pages/NotFound";
+
+export const router = createBrowserRouter([
+  {
+    path: "login",
+    element: <Login />,
+  },
+
+  {
+    element: <RequireLogin />,
+    children: [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          {
+            index: true,
+            element: <Client />,
+          },
+          {
+            path: "stock",
+            element: <Stock />,
+          },
+          {
+            path: "invoice",
+            element: <Invoice />,
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
