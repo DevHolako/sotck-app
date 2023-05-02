ad8056cb89b2b7c1cedb10e2fcbd149f651e66d4
DevHolako <holakonoob@gmail.com>
2023-05-02 19:53:15 +0100

    main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 function App() {
-  return <div>HEYYY</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
