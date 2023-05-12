4cc50d66e42ca527a7f8f7c6afdff0cffa6ca53d
DevHolako <holakonoob@gmail.com>
2023-05-12 23:48:11 +0100

    main changes

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,16 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { AuthProvider } from "react-auth-kit";
+import "rodal/lib/rodal.css";
+import { refreshApi } from "./Auth/RefreshApi.ts";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <AuthProvider
-        authName={"auth"}
-        authType="cookie"
-        cookieSecure={false}
-        cookieDomain={window.location.hostname}
-      >
+      <AuthProvider authName="jwt-aut" authType="cookie" refresh={refreshApi}>
         <App />
+        <ToastContainer />
       </AuthProvider>
     </Provider>
   </React.StrictMode>
