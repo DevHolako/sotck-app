12af3feed8deb313e0dcbfb821d9f0f7c4567640
DevHolako <holakonoob@gmail.com>
2023-05-18 22:12:13 +0100

    add new pkg's and some configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@features": path.resolve(__dirname, "./src/app/features"),
+      "@compo": path.resolve(__dirname, "./src/components"),
+      "@helpers": path.resolve(__dirname, "./src/helpers"),
+      "@hooks": path.resolve(__dirname, "./src/helpers/Hooks"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+    },
+  },
+});
